418ca09dd95ccd46069e95b8ca30e1675ac86838
Sasha_Drozdov <drsasha7@mail.ru>
2019-09-25 14:07:54 +0300

    Теперь есть параметр времени между выстрелами

--- Assets/scripts/Gun/GunShoot.cs
+++ Assets/scripts/Gun/GunShoot.cs
@@ -9,11 +9,23 @@ public class GunShoot : MonoBehaviour
 
     public GameObject bullet;
     public GameObject bullet_spawn_point;
+    public float wait_time;
+
+    private float cur_time = 0;
 
     //Methods
 
+    void Update(){
+        if (cur_time > 0){
+            cur_time -= Time.deltaTime;
+        }
+    }
+
     public void Shoot(){
-        Transform new_bullet = Instantiate(bullet.transform, bullet_spawn_point.transform.position, Quaternion.identity);
-        new_bullet.transform.rotation = transform.rotation;
+        if (cur_time <= 0){
+            cur_time = wait_time;
+            Transform new_bullet = Instantiate(bullet.transform, bullet_spawn_point.transform.position, Quaternion.identity);
+            new_bullet.transform.rotation = transform.rotation;
+        }
     }
 }
