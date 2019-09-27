115bf50bc96d6097dc01b55269b0a0a7e1adf981
Sasha_Drozdov <drsasha7@mail.ru>
2019-09-27 12:52:40 +0300

    Added health bar and some bullet properties

--- Assets/scripts/Bullet/BulletHit.cs
+++ Assets/scripts/Bullet/BulletHit.cs
@@ -6,13 +6,22 @@ public class BulletHit : MonoBehaviour
 {
 
     public float damage;
+    public float go_through;
     private GameObject killable;
 
     public void OnTriggerEnter(Collider other){
+        go_through -= 1;
         if (other.tag == "Killable"){
             killable = other.gameObject;
             killable.GetComponent<KillableHealth>().health -= damage;
         }
+        if (go_through <= 0){
+            Die();
+        }
+    }
+
+    public void Die(){
+        Destroy(this.gameObject);
     }
 
 }
--- Assets/scripts/Killable/KillableHealth.cs
+++ Assets/scripts/Killable/KillableHealth.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KillableHealth : MonoBehaviour{
     //Variables
 
     public float health;
+    public float max_health;
+
+    public Canvas hp_bar;
+    public float height;
 
     //Methods
+
+    void Start(){
+        health = max_health;
+    }
+
     void Update(){
+        //Rotating to Camera;
+
+        Quaternion target_rotation = Camera.main.transform.rotation;
+        target_rotation.x = target_rotation.x;
+        target_rotation.y = target_rotation.y;
+        target_rotation.z = target_rotation.z;
+        hp_bar.transform.rotation = target_rotation;
+
+        //Following the object
+        Vector3 sup = transform.position;
+        sup.y += height;
+        hp_bar.transform.position = sup;
+
+        //Synchronizing with HP
+
+        hp_bar.GetComponent<Image>().fillAmount = health / max_health;
         if (health <= 0){
             Die();
         }
     }
 
     void Die(){
+        Destroy(hp_bar);
         Destroy(this.gameObject);
     }
 }
